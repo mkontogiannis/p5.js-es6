ad1c6cd1307f316d5a10191d001cc06a76357fd7
michael kontogiannis <mkontogiannis@protonmail.com>
2021-03-28 20:09:34 +0200

    chore: updated deps + webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,58 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	devtool: "inline-sourcemap",
-	entry: __dirname + '/src/js/sketch.js',
-	output: {
-		path: __dirname + '/dist/',
-		publicPath: '/',
-		filename: 'js/sketch.js'
-	},
-	devServer: {
-		inline: true,
-		hot: true,
-		port: 3333
-	},
-	module: {
-		rules: [{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.scss$/,
-				loader: "style-loader!css-loader!sass-loader"
-			}
-		]
-	},
-	plugins: [
-		new CopyWebpackPlugin({
-			patterns: [{
-					from: 'src/index.html',
-					to: 'index.html'
-				},
-				{
-					from: 'src/assets',
-					to: 'assets'
-				}
-			],
-		})
-	]
-}
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: __dirname + '/src/js/sketch.js',
+  output: {
+    path: __dirname + '/dist/',
+    publicPath: '/',
+    filename: 'js/sketch.js',
+  },
+  devServer: {
+    compress: true,
+    inline: true,
+    hotOnly: true,
+    open: 'Google Chrome',
+    port: 3333,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/index.html',
+          to: 'index.html',
+        },
+        {
+          from: 'src/assets',
+          to: 'assets',
+        },
+      ],
+    }),
+  ],
+};
